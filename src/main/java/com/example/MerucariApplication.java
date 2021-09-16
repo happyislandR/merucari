b13133dc4a71a39b788a4349de702d3ca5b80494
happyislandR <masahiro.fukushima@rakus-partners.co.jp>
2021-09-16 11:29:18 +0900

    first commit

---
+++ src/main/java/com/example/MerucariApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerucariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerucariApplication.class, args);
+	}
+
+}
